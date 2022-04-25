6b7802453084a75802dfffe2aecccf77f0331684
Shubham <subhamhimself@gmail.com>
2022-04-25 13:38:58 +0530

    Lab 4

--- 200123060.cpp
+++ 200123060.cpp
@@ -1,174 +1,200 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-#define ll long long
-#define f(i, a, b) for(ll i = a; i < b; i++)
-
-int totalPageSize;
+#define pb push_back
 
 class Page{
-    Page* prevPage;
-    Page* nextPage;
-    int countOfRecords = 0;
-    int startAddressOfFreeSpace = 0;
-
-    int freeSpace;
-    map<ll, pair<ll, ll>> directory;
-    friend class Heap;
-    
+    int maxNumOfRecords;
+    int availableRecords;
+    int* array;
+    friend class File;
 public:
-    Page(){
-        freeSpace = totalPageSize - 4*4;
+    Page(int recordsPerPage){
+        array = new int[recordsPerPage];
+        maxNumOfRecords = recordsPerPage;
+        availableRecords = 0;
+    }
+
+    int availableRecordsInPage(){
+        return availableRecords;
     }
 };
 
-class Heap{
-    int numOfPages;
-    Page* firstPage;
-
+class File{
+    deque<Page*> pagesPtr;
 public:
-    Heap(){
-        numOfPages = 0;
-        firstPage = NULL;
+    void insertPagePtr(Page* page){
+        pagesPtr.pb(page);
+    }
+
+    Page* pointToPage(int pageIndex){
+        return pagesPtr[pageIndex];
+    }
+
+    void insertInPage(Page* currentPage, int posToInsert, int numToInsert){
+        currentPage->array[posToInsert] = numToInsert;
+        currentPage->availableRecords++;
+    }
+
+    void extSort(int B){
+        int pass = 0;
+        while(pagesPtr.size() > 1){
+            internalSort(pass, B);
+
+            pass++;
+        }
+
+    }
+
+    void internalSort( int pass, int B){
+        if(pass == 0){
+            int pageCount = 0;
+            int initialPagesPtrSize = pagesPtr.size();
+            int maxRecordsInNewPage = pagesPtr[0] -> maxNumOfRecords;
+
+            vector<int> temp;
+            while(pageCount <= initialPagesPtrSize){
+                //If pageCount % B == 0, then insert elements of sorted temp array in newPages.
+                if(pageCount % B == 0 || pageCount == initialPagesPtrSize){
+                    //sort temp array
+                    sort(temp.begin(), temp.end());
+
+                    //create new pages whenever previous page is full
+                        //insert the newPage in pagesPtr
+                        //insert the element in new page
+                    int i = 0;
+                    Page* currentPagePtr;
+                    while(i < temp.size()){
+
+                        if(i % maxRecordsInNewPage == 0){
+                            Page* newPage = new Page(maxRecordsInNewPage);
+                            currentPagePtr = newPage;
+                            pagesPtr.pb(newPage);
+                        }
+
+                        insertInPage(currentPagePtr, i % maxRecordsInNewPage, temp[i]);
+                        i++;
+
+                    }
+
+                    temp.clear();
+                }
+
+                if(pageCount != initialPagesPtrSize){
+                    //Otherwise, add the elemets of pagesPtr[0] to temp array.
+                    for(int i = 0; i < pagesPtr[0]->availableRecordsInPage(); i++){
+                        temp.pb(pagesPtr[0]->array[i]);
+                    }
+
+                    //delete the array which is pointed by pagesPtr[0] -> array;
+                    //remove its pointer from pagesPtr deque.
+                    delete pagesPtr[0] -> array;
+                    pagesPtr.pop_front();
+                }
+
+                //Go to the next page
+                pageCount++;
+            }
+        }else{
+            //For pass = 1,2,3...... do this :-
+            int pageCount = 0;
+            int initialPagesPtrSize = pagesPtr.size();
+
+            int numOfPagesToBeMerged = (B - 1);
+            int maxRecordsInNewPage = numOfPagesToBeMerged * (pagesPtr[0] -> maxNumOfRecords);
+
+            vector<int> temp;
+            while(pageCount <= initialPagesPtrSize){
+                //If pageCount % B == 0, then insert elements of sorted temp array in newPages.
+                if(pageCount % numOfPagesToBeMerged == 0 || pageCount == initialPagesPtrSize){
+                    //sort temp array
+                    sort(temp.begin(), temp.end());
+
+                    //create new pages whenever previous page is full
+                        //insert the newPage in pagesPtr
+                        //insert the element in new page
+                    int i = 0;
+                    Page* currentPagePtr;
+                    while(i < temp.size()){
+
+                        if(i % maxRecordsInNewPage == 0){
+                            Page* newPage = new Page(maxRecordsInNewPage);
+                            currentPagePtr = newPage;
+                            pagesPtr.pb(newPage);
+                        }
+
+                        insertInPage(currentPagePtr, i % maxRecordsInNewPage, temp[i]);
+                        i++;
+
+                    }
+                    temp.clear();
+                }
+                
+                if(pageCount != initialPagesPtrSize){
+                    //Otherwise, add the elemets of pagesPtr[0] to temp array.
+                    for(int i = 0; i < pagesPtr[0]->availableRecordsInPage(); i++){
+                        temp.pb(pagesPtr[0]->array[i]);
+                    }
+
+                    //delete the array which is pointed by pagesPtr[0] -> array;
+                    //remove its pointer from pagesPtr deque.
+                    delete pagesPtr[0] -> array;
+                    pagesPtr.pop_front();
+                }
+                //Go to the next page
+                pageCount++;
+            }
+        }
+        return;
     }
 
     void showStatus(){
-        if(numOfPages > 0){
-            cout<<numOfPages<<" ";
-
-            Page* currentPage = firstPage;
-
-            while(currentPage != NULL){
-                cout<<currentPage->countOfRecords<<" ";
-
-                currentPage = currentPage->nextPage;
-            }cout<<endl;
-        }else{
-            cout<<numOfPages<<endl;
+        int i = 0;
+        while(i < pagesPtr[0]->availableRecordsInPage()){
+            cout<<pagesPtr[0]->array[i]<<endl;
+            i++;
         }
-    }
-
-    void insert(int recordSize, int keyToInsert){
-        if(firstPage == NULL){
-            firstPage = new Page();
-
-            firstPage -> prevPage = NULL;
-            firstPage -> nextPage = NULL;
-            firstPage -> countOfRecords = 1;
-
-            firstPage -> directory[firstPage -> startAddressOfFreeSpace] = {keyToInsert, recordSize};
-            firstPage -> startAddressOfFreeSpace = firstPage -> startAddressOfFreeSpace + recordSize; // here, a +1 may be required to produce the correct output.
-            firstPage -> freeSpace = firstPage -> freeSpace - (recordSize + 4);
-            
-            numOfPages++;
-        }else{
-            Page* currentPage = firstPage;
-            Page* upcomingPage = NULL;
-            bool inserted = false;
-            
-            while(currentPage != NULL && inserted == false){
-                if(currentPage -> freeSpace >= recordSize + 4){
-                    //insert the record in currentPage
-
-                    currentPage -> countOfRecords++;
-                    
-                    currentPage -> directory[currentPage -> startAddressOfFreeSpace] = {keyToInsert, recordSize};
-                    currentPage -> startAddressOfFreeSpace = firstPage -> startAddressOfFreeSpace + recordSize;
-                    currentPage -> freeSpace = firstPage -> freeSpace - (recordSize + 4);
-
-                    inserted = true;
-                }else{
-                    if(currentPage->nextPage == NULL){
-                        //create a new Page.
-                        //Point the nextPage pointer of currentPage to the new Page.
-                        //Also point the prevPage pointer of new Page to currentPage.
-                        //insert the new Record.
-                        //increment the value of numOfPages.
-
-                        upcomingPage = new Page();
-
-                        upcomingPage -> prevPage = currentPage;
-                        upcomingPage -> nextPage = NULL;
-                        upcomingPage -> countOfRecords = 1;
-
-                        upcomingPage -> directory[upcomingPage -> startAddressOfFreeSpace] = {keyToInsert, recordSize};
-                        upcomingPage -> startAddressOfFreeSpace = upcomingPage -> startAddressOfFreeSpace + recordSize;
-                        upcomingPage -> freeSpace = upcomingPage -> freeSpace - (recordSize + 4);
-
-
-                        currentPage -> nextPage = upcomingPage;
-                        numOfPages++;
-
-                        inserted = true;
-                    }else{
-                        //change the value of "currentPage" to "nextPage".
-
-                        currentPage = currentPage -> nextPage;
-                    }
-                }
-                    
-            }
-            
-        }
-    }
-
-    void search(int keyToSearch){
-        Page* currentPage = firstPage;
-        int currentPageID = 0;
-        bool found = false;
-
-        while(currentPage != NULL && found == false){
-            int slotID = 0;
-
-            for(auto i: currentPage->directory){
-                if(i.second.first == keyToSearch){
-                    cout<<currentPageID<<" "<<slotID<<endl;
-                    found = true;
-                    break;
-                }
-                slotID++;
-            }
-
-            currentPage = currentPage -> nextPage;
-            currentPageID++;
-        }
-
-        if(!found){
-            cout<<-1<<" "<<-1<<endl;
-        }
-        
     }
 };
 
 int main(){
+    int B;  //Available Buffer Pages
+    cin>>B;
 
-    cin>>totalPageSize;
+    int numOfRecordsPerPage;
+    cin>>numOfRecordsPerPage;
 
-    Heap heap;
+    int totalNumOfRecords;
+    cin>>totalNumOfRecords;
 
-    while(1){
-        int x;
-        cin>>x;
+    int numOfPages = (totalNumOfRecords / numOfRecordsPerPage);
+    //Number of pages required is equal to (totalNumOfRecords/numOfRecordsPerPage)
+    (totalNumOfRecords % numOfRecordsPerPage) > 0 ? numOfPages += 1 : numOfPages = numOfPages;
 
-        if(x == 1){
-            int recordSize;
-            int keyToInsert;
+    File file;
 
-            cin>>recordSize>>keyToInsert;
-
-            heap.insert(recordSize, keyToInsert);
-        }else if(x == 2){
-            heap.showStatus();
-        }else if(x == 3){
-            int keyToSearch;
-
-            cin>>keyToSearch;
-
-            heap.search(keyToSearch);
-        }else{
-            cout<<"Quitting the program..."<<endl;
-            break;
-        }
+    for(int i = 0; i < numOfPages; i++){
+        Page* newPage = new Page(numOfRecordsPerPage);
+        file.insertPagePtr(newPage);
     }
+
+    int cnt = 0;
+    Page* currentPagePtr;
+    for(int i = 0; i < totalNumOfRecords; i++){
+        if(cnt % numOfRecordsPerPage == 0){
+            int pageIndex = cnt / numOfRecordsPerPage;
+            currentPagePtr = file.pointToPage(pageIndex);
+        }
+
+        int numToInsert;
+        cin>>numToInsert;
+
+        file.insertInPage(currentPagePtr, cnt % numOfRecordsPerPage, numToInsert);
+        cnt++;
+    }
+
+    file.extSort(B);
+    file.showStatus();
+
+    return 0;
 }
